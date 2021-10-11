76cf0b1ce3429094986351916d37f2d12bc1275d
Gfeller Martin <mgf@acm.org>
2021-10-11 09:51:23 +0200

    Data manipulations

---
+++ tests/test_datautils.py
@@ -0,0 +1,43 @@
+import pytest
+from core import keys
+from utils import datautils
+
+@pytest.fixture
+def structure1():
+    d = {'mgf':
+            {
+            'dob': 1620212,
+            },
+        
+        'lise':
+            {
+            'a': 'bbb',
+            'c': 23345.5,
+            },
+        }
+    d['lise']['family'] = d
+    return d
+
+def test_extract(structure1):
+    assert datautils.extract_data(structure1,keys.DDHkey('mgf/dob')) == structure1['mgf']['dob']
+    assert datautils.extract_data(structure1,keys.DDHkey('lise/family/mgf/dob')) == structure1['mgf']['dob']
+    assert datautils.extract_data(structure1,keys.DDHkey('lise/family/unknown'),default=None) is None
+    with pytest.raises(KeyError):
+        assert datautils.extract_data(structure1,keys.DDHkey('lise/family/unknown'))
+    with pytest.raises(ValueError):
+        assert datautils.extract_data(structure1,keys.DDHkey('lise/family/unknown'),raise_error=ValueError)
+    return
+
+def test_insert(structure1):
+    s2 = datautils.insert_data(structure1,keys.DDHkey('mgf/partner'),structure1['lise'])
+    assert datautils.extract_data(s2,keys.DDHkey('mgf/partner')) == datautils.extract_data(s2,keys.DDHkey('lise'))
+    assert datautils.extract_data(s2,keys.DDHkey('mgf/partner/family/mgf/dob')) == structure1['mgf']['dob']
+    with pytest.raises(KeyError):
+        datautils.insert_data(structure1,keys.DDHkey('unknown/partner'),structure1['lise'])
+
+def test_split(structure1):
+    s2 = datautils.insert_data(structure1,keys.DDHkey('mgf/partner'),structure1['lise'])
+    above,below = datautils.split_data(s2,keys.DDHkey('mgf/partner'))
+    assert not datautils.extract_data(above,keys.DDHkey('mgf/partner')) 
+    assert below == structure1['lise']
+    assert datautils.extract_data(above,keys.DDHkey('mgf/dob')) == structure1['mgf']['dob']
--- utils/datautils.py
+++ utils/datautils.py
@@ -1,9 +1,29 @@
-""" Data manipulations """
+""" Data manipulations. Manipulate dictionaries using glom.
+"""
+import glom
+from core import keys
+import copy
 
-    
+class _missing: ...
+missing = _missing()
 
-def splitdata(data,remainder) -> tuple:
+def extract_data(data : dict,subkey : keys.DDHkey,default=missing,raise_error=KeyError) -> dict:
+    """ extract data at subkey """
+    data = glom.glom(data,subkey.key,default=default)
+    if data is missing:
+        raise raise_error(subkey)
+    return data
+
+def insert_data(data : dict,subkey : keys.DDHkey, newdata : dict,raise_error=KeyError) -> dict:
+    """ insert newdata into data at subkey """
+    glom.glom(data,glom.Assign('.'.join(subkey.key),newdata))
+    return data
+
+
+def split_data(data,subkey : keys.DDHkey,raise_error=KeyError) -> tuple[dict,dict]:
     """ split data so that everything in remainder is below, everything else above """
-    below = data # TODO!
-    above = None
+    gkey = '.'.join(subkey.key)
+    below = glom.glom(data,gkey)
+    above = copy.copy(data)
+    glom.glom(above,glom.Assign(gkey,None))
     return above,below
