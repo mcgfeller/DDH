2bb650e28695a20386b11830b3b206b2f2981b2f
Gfeller Martin <mgf@acm.org>
2022-03-25 11:15:09 +0100

    Multiple Schemas per DApp

--- core/dapp.py
+++ core/dapp.py
@@ -79,7 +79,7 @@ class DApp(DAppOrFamily):
     
     @classmethod
     def get_references(cls):
-        """ retrutn references; can be overwritten """
+        """ return references; can be overwritten """
         return cls.references
 
 
@@ -87,34 +87,36 @@ class DApp(DAppOrFamily):
     def bootstrap(cls,session) -> DApp:
         return cls()
 
-    def startup(self,session)  -> nodes.Node:
-        dnode = self.check_registry(session)
-        return dnode
+    def startup(self,session)  -> list[nodes.Node]:
+        dnodes = self.check_registry(session)
+        return dnodes
 
-    def check_registry(self,session) -> nodes.Node:
+    def check_registry(self,session) -> list[nodes.Node]:
         transaction = session.get_or_create_transaction()
-        dnode = keydirectory.NodeRegistry[self.schemakey].get(nodes.NodeSupports.schema) # need exact location, not up the tree
-        if dnode:
-            dnode = dnode.ensure_loaded(transaction)
-        else:
-            # get a parent scheme to hook into
-            upnode,split = keydirectory.NodeRegistry.get_node(self.schemakey,nodes.NodeSupports.schema,transaction)
-            pkey = self.schemakey.up()
-            if not pkey:
-                raise ValueError(f'{self.schemakey} key is too high {self!r}')
-            upnode = typing.cast(nodes.SchemaNode,upnode)
-            parent = upnode.get_sub_schema(pkey,split)
-            if not parent:
-                raise ValueError(f'No parent schema found for {self!r} with {self.schemakey} at upnode {upnode}')
-            schema = self.get_schema() # obtain static schema
-            # give world schema_read access
-            consents = permissions.Consents(consents=[permissions.Consent(grantedTo=[principals.AllPrincipal],withModes={permissions.AccessMode.schema_read})])
-            dnode = DAppNode(owner=self.owner,schema=schema,dapp=self,consents=consents)
-            keydirectory.NodeRegistry[self.schemakey] = dnode
-            # now insert our schema into the parent's:
-            schemaref = schemas.SchemaReference.create_from_key(self.__class__.__name__,ddhkey=self.schemakey)
-            parent.add_fields({self.schemakey[-1] : (schemaref,None)})
-        return dnode 
+        dnodes = []
+        for schemakey,schema in self.get_schemas().items():
+            dnode = keydirectory.NodeRegistry[schemakey].get(nodes.NodeSupports.schema) # need exact location, not up the tree
+            if dnode:
+                dnode = dnode.ensure_loaded(transaction)
+            else:
+                # get a parent scheme to hook into
+                upnode,split = keydirectory.NodeRegistry.get_node(schemakey,nodes.NodeSupports.schema,transaction)
+                pkey = schemakey.up()
+                if not pkey:
+                    raise ValueError(f'{schemakey} key is too high {self!r}')
+                upnode = typing.cast(nodes.SchemaNode,upnode)
+                parent = upnode.get_sub_schema(pkey,split)
+                if not parent:
+                    raise ValueError(f'No parent schema found for {self!r} with {schemakey} at upnode {upnode}')
+                # give world schema_read access
+                consents = permissions.Consents(consents=[permissions.Consent(grantedTo=[principals.AllPrincipal],withModes={permissions.AccessMode.schema_read})])
+                dnode = DAppNode(owner=self.owner,schema=schema,dapp=self,consents=consents)
+                keydirectory.NodeRegistry[schemakey] = dnode
+                # now insert our schema into the parent's:
+                schemaref = schemas.SchemaReference.create_from_key(self.__class__.__name__,ddhkey=schemakey)
+                parent.add_fields({schemakey[-1] : (schemaref,None)})
+            dnodes.append(dnode)
+        return dnodes 
     
 
     @abstractmethod
