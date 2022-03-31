1bd974cd6f270b0238c0b6590a8f3078583b965a
Gfeller Martin <mgf@acm.org>
2022-03-31 13:13:03 +0200

    Use venv

--- runapi.cmd
+++ runapi.cmd
@@ -1,1 +1,4 @@
-"C:\Program Files\Python39\scripts\uvicorn" frontend.dapp_api:app --port 8001 --reload --no-use-colors
+cd %~dp0
+rem .venv\scripts\activate
+rem "C:\Program Files\Python39\scripts\uvicorn" frontend.dapp_api:app --port 8001 --reload --no-use-colors
+.venv\scripts\uvicorn frontend.dapp_api:app --port 8001 --reload --no-use-colors
