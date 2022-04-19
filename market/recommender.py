058fc631995378ab715c4d1f6d2fc0844a604d61
Gfeller Martin <mgf@acm.org>
2022-04-19 12:15:18 +0200

    Basic market query and tests

--- market/recommender.py
+++ market/recommender.py
@@ -3,28 +3,80 @@
 from __future__ import annotations
 import typing
 import logging
+import operator
 
 logger = logging.getLogger(__name__)
 
+import pydantic
 from utils import utils
 from core import schema_network, schema_root,dapp,principals,keys,pillars
+from user import subscriptions
 from utils.pydantic_utils import NoCopyBaseModel
 
 
+class SearchResultItem(NoCopyBaseModel):
+    """ a single search result, with some search information """
+    da : dapp.DApp = pydantic.Field(alias='dapp')
+    cost : float = 0.0
+    ignored_labels : typing.Iterable[str] = [] # query labels that have been ignored
+    merit : int = pydantic.Field(0,description="Ranking merit, starts at 0")
 
-def search_dapps(session,query):
-    dapps = pillars.DAppManager.DAppsById.values()
+
+
+def search_dapps(session,query : typing.Optional[str],categories : typing.Optional[typing.Iterable[common_ids.CatalogCategory]],desired_labels : typing.Optional[typing.Iterable[common_ids.Label]]) -> list[SearchResultItem]:
+    subscribed = subscriptions.list_subscriptions(session.user)
     if query:
+        dapps = dapps_in_categories(session,categories)
         dapps = search_text(session,dapps,query)
-    dapps = order_dapps(session,dapps)
-    return dapps
+    elif categories:
+        dapps = dapps_in_categories(session,categories)
+    elif subscribed: # no input, propose complementing to subscribed
+        dapps = from_subscribed(session,subscribed)
+    else: # no input at all - currently, list all DApps - but may raise 413 later 
+        dapps = pillars.DAppManager.DAppsById.values()
+    sris = [SearchResultItem(dapp=da) for da in dapps]
+    if desired_labels:
+        sris = check_labels(session,sris,frozenset(desired_labels))
+    sris = add_costs(session,sris)
+    sris = grade_results(session,sris)
+
+    return sris
+
+def dapps_in_categories(session,categories):
+    if categories:
+        categories = frozenset(categories)
+        return (da for da in pillars.DAppManager.DAppsById.values() if da.catalog in categories)
+    else:
+        return pillars.DAppManager.DAppsById.values()
 
 def search_text(session,dapps,query):
-    dapps = (d for d in dapps if query.lower() in d.description.lower()) # TODO: Real search
+    dapps = (d for d in dapps if query.lower() in d.searchtext) # TODO: Real search
     return dapps
 
-def order_dapps(session,dapps):
+
+
+def from_subscribed(session,dapps : typing.Iterable[dapp.DApp]) -> dict[dapp.DApp,list[dapp.DApp]]:
+    """ all reachable Data Apps from subscribed Data Apps, with cost of reach """
     schemaNetwork = pillars.Pillars['SchemaNetwork']
-    dapps = list(dapps) # XXX
+    reachs = {d:schemaNetwork.dapps_reachable(d,session.user) for d in dapps if isinstance(d,dapp.DApp)}
+    return {da:[da] for da in dapps}
+
+def required(session,dapps : typing.Iterable[dapp.DApp]) -> dict[dapp.DApp,list[dapp.DApp]]:
+    """ return all Data Apps that are required by Dapps, per Dapp """
+    schemaNetwork = pillars.Pillars['SchemaNetwork']
     deps = {d:schemaNetwork.dapps_required(d,session.user) for d in dapps if isinstance(d,dapp.DApp)}
-    return  dapps
+    return deps
+
+def check_labels(session,sris : list[SearchResultItem],desired_labels : set[common_ids.Label]) -> list[SearchResultItem]:
+    """ check presence of labels: Mark missing labels and demerit for each missing label  """
+    for sri in sris:
+        sri.ignored_labels = desired_labels.difference(sri.da.labels)
+        sri.merit -= len(sri.ignored_labels)
+    return sris
+
+def add_costs(session,sris : list[SearchResultItem]) -> list[SearchResultItem]:
+    # TODO
+    return sris
+
+def grade_results(session,sris : list[SearchResultItem]) -> list[SearchResultItem]:
+    return sorted(sris,key=operator.attrgetter('merit'),reverse=True) # highest merit first
--- tests/test_recommender.py
+++ tests/test_recommender.py
@@ -1,5 +1,5 @@
 """ Set up some Test data """
-from core import keys,permissions,facade,errors,principals
+from core import keys,permissions,facade,errors,principals,common_ids
 from core import pillars
 from frontend import user_auth,sessions
 from market import recommender
@@ -13,12 +13,61 @@ def user():
 def session(user):
     return sessions.Session(token_str='test_session',user=user)
 
-def test_query(user,session):
-    """ test retrieval of key of test MigrosDApp, and facade.get_schema() """
-    dapps = recommender.search_dapps(session,query='tax')
-    assert dapps
+def test_all(user,session):
+    """ Test withhout criteria """
+    sris = recommender.search_dapps(session,query=None,categories=None,desired_labels=None)
+    assert len(sris) == len(pillars.DAppManager.DAppsById)
     return
 
+def test_query_1(user,session):
+    """ Test simple text query """
+    sris = recommender.search_dapps(session,query='tax',categories=None,desired_labels=None)
+    assert sris
+    return
+
+def test_query_label_1(user,session):
+    """ Simple text + fulfilled label """
+    sris = recommender.search_dapps(session,query='tax',categories=None,desired_labels=[common_ids.Label.free])
+    assert sris
+    return
+
+def test_query_label_0(user,session):
+    """  Simple text + non-fulfilled label """
+    sris = recommender.search_dapps(session,query='tax',categories=None,desired_labels=[common_ids.Label.anonymous])
+    assert sris
+    assert sris[0].merit < 0
+    assert common_ids.Label.anonymous in sris[0].ignored_labels
+    return
+
+def test_query_cat_1(user,session):
+    """ Test simple text query """
+    sris = recommender.search_dapps(session,query='tax',categories=[common_ids.CatalogCategory.finance],desired_labels=None)
+    assert sris
+    return
+
+def test_query_cat_label_1(user,session):
+    """ Test simple text query """
+    sris = recommender.search_dapps(session,query='tax',categories=[common_ids.CatalogCategory.finance],desired_labels=[common_ids.Label.free])
+    assert sris
+    return
+
+def test_query_cat_0(user,session):
+    """ Test simple text query """
+    sris = recommender.search_dapps(session,query='tax',categories=[common_ids.CatalogCategory.health],desired_labels=None)
+    assert not sris,'tax is not in health'
+    return
+
+def test_cat_1(user,session):
+    """ Test simple catalog query """
+    sris = recommender.search_dapps(session,query=None,categories=[common_ids.CatalogCategory.living],desired_labels=None)
+    assert sris
+
+def test_cat_label(user,session):
+    """ Test simple catalog query """
+    sris = recommender.search_dapps(session,query=None,categories=[common_ids.CatalogCategory.living],desired_labels=[common_ids.Label.anonymous])
+    assert sris
+
+pillars.DAppManager.DAppsById
 
 if __name__ == '__main__':
-    test_query(user,session)
+    test_all(user,session)
