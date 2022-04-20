46f408ac59c47bd922587211c9b0c044a26a6cf5
Gfeller Martin <mgf@acm.org>
2022-04-20 16:18:11 +0200

    Subscription changes return list of subscriptions after change

--- user/subscriptions.py
+++ user/subscriptions.py
@@ -2,7 +2,7 @@
 
 import typing
 
-from core import pillars, principals,common_ids,errors
+from core import pillars, principals,common_ids,errors,dapp
 
 
 SUBSCRIPTIONS : dict[common_ids.PrincipalId,dict[principals.DAppId,typing.Any]] = {}
@@ -10,16 +10,28 @@ SUBSCRIPTIONS : dict[common_ids.PrincipalId,dict[principals.DAppId,typing.Any]] 
 def add_subscription(
     user: common_ids.PrincipalId,
     dappid : principals.DAppId,
-    ):
+    ) -> list[principals.DAppId]:
 
     da = pillars.DAppManager.DAppsById.get(dappid)
     if not da:
         raise errors.NotFound(f"DApp not found: {dappid}.")
     SUBSCRIPTIONS.setdefault(user,{})[dappid] = {}
-    return
+    return list(SUBSCRIPTIONS[user].keys())
+
+def delete_subscription(
+    user: common_ids.PrincipalId,
+    dappid : principals.DAppId,
+    ) -> list[principals.DAppId]:
+
+    da = pillars.DAppManager.DAppsById.get(dappid)
+    if not da:
+        raise errors.NotFound(f"DApp not found: {dappid}.")
+    SUBSCRIPTIONS.setdefault(user,{}).pop(dappid,None)
+    return list(SUBSCRIPTIONS[user].keys())
 
 def list_subscriptions(
     user: common_ids.PrincipalId,
-    ):
+    ) -> list[dapp.DApp]:
     dappids = SUBSCRIPTIONS.setdefault(user,{}).keys()
     das = [pillars.DAppManager.DAppsById.get(dappid) for dappid in dappids]
+    return das
