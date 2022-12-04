9c21f8cb183e84ffeb33ab5bef27bef815b84aaa
Gfeller Martin <mgf@acm.org>
2022-12-04 13:15:03 +0100

    Split test_core into keys and nodes

---
+++ tests/test_keys.py
@@ -0,0 +1,30 @@
+from core import keys
+import pytest
+
+def test_paths():
+    ddhkey1 = keys.DDHkey(key='norooted')
+    ddhkey2 = keys.DDHkey(key='norooted/subkey')
+    assert ddhkey1 == ddhkey2.up()
+
+    ddhkey3 = keys.DDHkey(key='/rooted')
+    ddhkey4 = keys.DDHkey(key='/rooted/subkey')
+    assert ddhkey3 == ddhkey4.up()
+    assert ddhkey3.up().up() is None
+    ddhkey = keys.DDHkey(key=())
+
+    return
+
+def test_forks():
+    ddhkey1 = keys.DDHkey(key='norooted:schema')
+    ddhkey2 = keys.DDHkey(key='norooted/subkey:schema')
+    assert ddhkey1 == ddhkey2.up()  
+
+    ddhkey3 = keys.DDHkey(key='norooted/subkey')   
+    assert ddhkey1 != ddhkey3.up()  # forks don't match
+
+    ddhkey4 = keys.DDHkey(key='norooted/subkey:data')  
+    assert ddhkey3 == ddhkey4 
+
+    with pytest.raises(ValueError):
+        keys.DDHkey(key='norooted/subkey:stupid')  # invalid key
+        
--- tests/test_nodes.py
+++ tests/test_nodes.py
@@ -1,34 +1,6 @@
 from core import keys,nodes,permissions,schemas,facade,keydirectory,transactions,principals
-from . import  test_dapp_data
 from frontend import sessions
-import pytest
 
-def test_paths():
-    ddhkey1 = keys.DDHkey(key='norooted')
-    ddhkey2 = keys.DDHkey(key='norooted/subkey')
-    assert ddhkey1 == ddhkey2.up()
-
-    ddhkey3 = keys.DDHkey(key='/rooted')
-    ddhkey4 = keys.DDHkey(key='/rooted/subkey')
-    assert ddhkey3 == ddhkey4.up()
-    assert ddhkey3.up().up() is None
-    ddhkey = keys.DDHkey(key=())
-
-    return
-
-def test_forks():
-    ddhkey1 = keys.DDHkey(key='norooted:schema')
-    ddhkey2 = keys.DDHkey(key='norooted/subkey:schema')
-    assert ddhkey1 == ddhkey2.up()  
-
-    ddhkey3 = keys.DDHkey(key='norooted/subkey')   
-    assert ddhkey1 != ddhkey3.up()  # forks don't match
-
-    ddhkey4 = keys.DDHkey(key='norooted/subkey:data')  
-    assert ddhkey3 == ddhkey4 
-
-    with pytest.raises(ValueError):
-        keys.DDHkey(key='norooted/subkey:stupid')  # invalid key
         
 
 class DummyElement(schemas.SchemaElement): ...
