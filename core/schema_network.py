c1d3f6a6bc5c567a6fc44101e6d47150dc47cdce
Gfeller Martin <mgf@acm.org>
2022-12-05 16:43:46 +0100

    Formatting

--- core/schema_network.py
+++ core/schema_network.py
@@ -10,9 +10,7 @@ import matplotlib.pyplot as plt
 logger = logging.getLogger(__name__)
 
 from utils import utils
-from core import dapp_attrs,schemas,principals
-
-
+from core import dapp_attrs, schemas, principals
 
 
 class SchemaNetworkClass():
@@ -21,20 +19,23 @@ class SchemaNetworkClass():
         self.network = networkx.DiGraph()
         self.valid = utils.Invalidatable(self.complete_graph)
 
-    def plot(self,layout='circular_layout'):
+    def plot(self, layout='circular_layout'):
         """ Plot the current network """
         self.valid.use()
-        labels = {node : f"{attrs['type']}:{attrs['id']}" for node,attrs in self.network.nodes.items()} # short id for nodes
-        colors = ['blue' if attrs['type'] == 'schema' else 'red' for attrs in self.network.nodes.values()]
-        flayout = getattr(networkx,layout)
-        networkx.draw_networkx(self.network,pos=flayout(self.network),with_labels=True,labels=labels,node_color=colors)
+        labels = {node: f"{attrs['type']}:{attrs['id']}" for node,
+                  attrs in self.network.nodes.items()}  # short id for nodes
+        colors = ['blue' if attrs['type'] ==
+                  'schema' else 'red' for attrs in self.network.nodes.values()]
+        flayout = getattr(networkx, layout)
+        networkx.draw_networkx(self.network, pos=flayout(self.network),
+                               with_labels=True, labels=labels, node_color=colors)
         plt.show()
 
-    def dapps_from(self,from_dapp : dapp_attrs.DApp, principal : principals.Principal) -> typing.Iterable[dapp_attrs.DApp]: 
+    def dapps_from(self, from_dapp: dapp_attrs.DApp, principal: principals.Principal) -> typing.Iterable[dapp_attrs.DApp]:
         self.valid.use()
-        return [n for n in networkx.descendants(self.network,from_dapp) if isinstance(n,dapp_attrs.DApp)]
+        return [n for n in networkx.descendants(self.network, from_dapp) if isinstance(n, dapp_attrs.DApp)]
 
-    def dapps_required(self,for_dapp : dapp_attrs.DApp, principal : principals.Principal) -> tuple[set[dapp_attrs.DApp],set[dapp_attrs.DApp]]: 
+    def dapps_required(self, for_dapp: dapp_attrs.DApp, principal: principals.Principal) -> tuple[set[dapp_attrs.DApp], set[dapp_attrs.DApp]]:
         """ return two sets of DApps required by this DApp
             -   all required 
             -   required for cost calculation, considering despite schemas.Requires annotations, for which
@@ -43,50 +44,52 @@ class SchemaNetworkClass():
         """
         self.valid.use()
         g = self.network
-        sp = networkx.shortest_path(g, target = for_dapp)
-        
-        lines = [{x for x in l if isinstance(x,dapp_attrs.DApp)} for l in sp.values()]
+        sp = networkx.shortest_path(g, target=for_dapp)
+
+        lines = [{x for x in l if isinstance(x, dapp_attrs.DApp)} for l in sp.values()]
         suggested = set.union(*lines)
 
         # node: req if node has any requirement:
-        nodes_with_reqs = {k:req for k in sp.keys() if (nk:=g.nodes[k]) ['type'] == 'schema' and (req:=nk.get('requires'))}
+        nodes_with_reqs = {k: req for k in sp.keys() if (
+            nk := g.nodes[k])['type'] == 'schema' and (req := nk.get('requires'))}
         if nodes_with_reqs:
-            discard = set() # lines to be discarded, cumulative for all nodes
+            discard = set()  # lines to be discarded, cumulative for all nodes
             for node in nodes_with_reqs:
                 req = nodes_with_reqs[node]
-                if req in (schemas.Requires.one,schemas.Requires.specific,schemas.Requires.few): # use longest line unless .all
+                # use longest line unless .all
+                if req in (schemas.Requires.one, schemas.Requires.specific, schemas.Requires.few):
                     # len, line if attributed schema is in the path:
-                    ll = [(len(v),i) for i,(k,v) in enumerate(sp.items())  if node in v]
-                    retain = max(ll) # longest line (higher index for ties)
-                    discard.update({d[1] for d in ll if retain != d}) # others are to be discarded
-            lines = [l for i,l in enumerate(lines) if i not in discard] # all lines except those discarded
+                    ll = [(len(v), i) for i, (k, v) in enumerate(sp.items()) if node in v]
+                    retain = max(ll)  # longest line (higher index for ties)
+                    discard.update({d[1] for d in ll if retain != d})  # others are to be discarded
+            # all lines except those discarded
+            lines = [l for i, l in enumerate(lines) if i not in discard]
             calculated = set.union(*lines)
-        else: # no attributes, we use all nodes
+        else:  # no attributes, we use all nodes
             calculated = suggested
-        return suggested,calculated
+        return suggested, calculated
 
     def complete_graph(self):
         """ Finish up the graph after all nodes have been added:
-            
+
             Keys are provided if key above it is provided.
             For all keys that are requires, 
             check if a parent key is provided, and add a provision from this key to that parent
-            
+
         """
         # schema nodes that have an out_edge of type 'requires':
-        required_nodes = {n for n,n_out,t in self.network.out_edges((node for node,typ in self.network.nodes(data='type',default=None) if typ =='schema'),data='type') if t=='requires'} # type:ignore
+        required_nodes = {n for n, n_out, t in self.network.out_edges((node for node, typ in self.network.nodes(
+            data='type', default=None) if typ == 'schema'), data='type') if t == 'requires'}  # type:ignore
 
         # schema nodes that have in in_edge of type requires
-        provided_nodes = {n for n_in,n,t in self.network.in_edges((node for node,typ in self.network.nodes(data='type',default=None) if typ =='schema'),data='type') if t=='provides'}  # type:ignore
+        provided_nodes = {n for n_in, n, t in self.network.in_edges((node for node, typ in self.network.nodes(
+            data='type', default=None) if typ == 'schema'), data='type') if t == 'provides'}  # type:ignore
 
-        for node in required_nodes: 
-            up = node 
-            while up:= up.up(): # go path up until exhausted
-                if up in provided_nodes: # provided at this level?
-                    self.network.add_edge(up,node,type='provides') # extend provision from up level to node
+        for node in required_nodes:
+            up = node
+            while up := up.up():  # go path up until exhausted
+                if up in provided_nodes:  # provided at this level?
+                    # extend provision from up level to node
+                    self.network.add_edge(up, node, type='provides')
 
         return
-
-
-
-
--- core/transactions.py
+++ core/transactions.py
@@ -10,7 +10,7 @@ import pydantic
 from pydantic.errors import PydanticErrorMixin
 from utils.pydantic_utils import NoCopyBaseModel
 
-from core import permissions,errors,principals,common_ids
+from core import permissions, errors, principals, common_ids
 
 import secrets
 
@@ -18,41 +18,44 @@ import secrets
 class TrxAccessError(errors.AccessError): ...
 
 
-DefaultReadConsentees = {principals.AllPrincipal.id} # by default, nothing is readable by everybody
+DefaultReadConsentees = {principals.AllPrincipal.id}  # by default, nothing is readable by everybody
+
 
 class Transaction(NoCopyBaseModel):
-    trxid : common_ids.TrxId 
+    trxid: common_ids.TrxId
     for_user: principals.Principal
     accesses: list[permissions.Access] = pydantic.Field(default_factory=list)
-    exp : datetime.datetime = datetime.datetime.now()
+    exp: datetime.datetime = datetime.datetime.now()
 
-    read_consentees : set[common_ids.PrincipalId] = DefaultReadConsentees # with nothing read, the world has access
-    initial_read_consentees :  set[common_ids.PrincipalId] = DefaultReadConsentees # same as read_consentees, but not modified during transaction
+    # with nothing read, the world has access
+    read_consentees: set[common_ids.PrincipalId] = DefaultReadConsentees
+    # same as read_consentees, but not modified during transaction
+    initial_read_consentees:  set[common_ids.PrincipalId] = DefaultReadConsentees
 
-    Transactions : typing.ClassVar[dict[common_ids.TrxId,typing.Any]] = {}  # https://github.com/pydantic/pydantic/issues/3679#issuecomment-1337575645 
+    # https://github.com/pydantic/pydantic/issues/3679#issuecomment-1337575645
+    Transactions: typing.ClassVar[dict[common_ids.TrxId, typing.Any]] = {}
     # Transactions : typing.ClassVar[dict[common_ids.TrxId,'Transaction']] = {}
-    TTL : typing.ClassVar[datetime.timedelta] = datetime.timedelta(seconds=30) # max duration of a transaction in seconds
+    TTL: typing.ClassVar[datetime.timedelta] = datetime.timedelta(
+        seconds=30)  # max duration of a transaction in seconds
 
-    def __init__(self,**kw):
+    def __init__(self, **kw):
         super().__init__(**kw)
         self.read_consentees = self.initial_read_consentees
         return
 
-
     @classmethod
-    def create(cls,for_user : principals.Principal,**kw) -> Transaction:
+    def create(cls, for_user: principals.Principal, **kw) -> Transaction:
         """ Create Trx, and begin it """
         trxid = secrets.token_urlsafe()
         if trxid in cls.Transactions:
             raise KeyError(f'duplicate key: {trxid}')
-        trx = cls(trxid=trxid,for_user=for_user,**kw)
+        trx = cls(trxid=trxid, for_user=for_user, **kw)
         trx.begin()
         return trx
 
     @classmethod
-    def get(cls,trxid : common_ids.TrxId) -> Transaction:
+    def get(cls, trxid: common_ids.TrxId) -> Transaction:
         return cls.Transactions[trxid].use()
-
 
     def begin(self):
         """ begin this transaction """
@@ -62,7 +65,7 @@ class Transaction(NoCopyBaseModel):
 
     def end(self):
         """ end this transaction """
-        self.Transactions.pop(self.trxid,None)
+        self.Transactions.pop(self.trxid, None)
         return
 
     def abort(self):
@@ -70,18 +73,17 @@ class Transaction(NoCopyBaseModel):
 
     def __del__(self):
         self.abort()
-    
+
     def use(self):
         if datetime.datetime.now() > self.exp:
             raise TrxAccessError(f'Transaction has expired; {self.TTL=}')
         return self
 
-
-    def add_and_validate(self, access : permissions.Access):
+    def add_and_validate(self, access: permissions.Access):
         """ add an access and validate whether it is ok """
         self.accesses.append(access)
-        if permissions.AccessMode.write in access.modes: # we must check writes for presence of read objects
-            if principals.AllPrincipal.id not in self.read_consentees and  access.ddhkey.owners not in self.read_consentees:
+        if permissions.AccessMode.write in access.modes:  # we must check writes for presence of read objects
+            if principals.AllPrincipal.id not in self.read_consentees and access.ddhkey.owners not in self.read_consentees:
                 msg = f'transactions contains data with no consent to use for {access.ddhkey.owners}'
                 if principals.AllPrincipal.id not in self.initial_read_consentees and access.ddhkey.owners not in self.initial_read_consentees:
                     # this transaction contains data from previous transaction, must reinit
@@ -96,4 +98,3 @@ class Transaction(NoCopyBaseModel):
         else:
             self.read_consentees &= read_consentees
         return
-
