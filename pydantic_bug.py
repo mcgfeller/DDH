88cd1b050eecc6204f49b5a39f3dbd35ca11356d
Gfeller Martin <mgf@acm.org>
2022-12-27 15:21:43 +0100

    Turn-off Pydantic workaround

--- pydantic_bug.py
+++ pydantic_bug.py
@@ -5,10 +5,17 @@ from __future__ import annotations
 import pydantic
 import typing
 
+WithClassVar = typing.ForwardRef('WithClassVar')  # resolved problem
+
+
 class WithClassVar(pydantic.BaseModel):
-    Instances : typing.ClassVar[dict[str,WithClassVar]] = {} # fails with issubclass() arg 1 must be a class; arg  1 is typing.ClassVar object 
+    # fails with issubclass() arg 1 must be a class; arg  1 is typing.ClassVar object
+    Instances: typing.ClassVar[dict[str, WithClassVar]] = {}
     # Instances : typing.ClassVar[dict[str,'WithClassVar']] = {} # worked in 1.9, stopped working in 1.10.2
     # instance : dict[str,WithClassVar] = {} # no classvar - works
+    i: int = 0
 
 
-
+WithClassVar.update_forward_refs()
+wcv = WithClassVar(i=42)
+print(wcv)
