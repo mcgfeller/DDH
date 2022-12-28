83471144e1fa435b64db38344d0b4ebcf9e498e5
Gfeller Martin <mgf@acm.org>
2022-12-28 10:46:35 +0100

    Introduce DApp Priviledges

--- core/dapp_attrs.py
+++ core/dapp_attrs.py
@@ -5,7 +5,7 @@ import enum
 import typing
 import pydantic
 
-from core import keys,schemas,policies,principals,relationships,common_ids,versions,permissions,transactions
+from core import keys, schemas, policies, principals, relationships, common_ids, versions, permissions, transactions
 from utils.pydantic_utils import NoCopyBaseModel
 
 
@@ -14,24 +14,24 @@ class DAppOrFamily(NoCopyBaseModel):
     class Config:
         extra = 'ignore'
 
-    id : str|None  = None # principals.DAppId causes Pydantic errors - I don't know why
-    description : str|None = None
-    owner : typing.ClassVar[principals.Principal] 
+    id: str | None = None  # principals.DAppId causes Pydantic errors - I don't know why
+    description: str | None = None
+    owner: typing.ClassVar[principals.Principal]
     policy: policies.Policy = policies.EmptyPolicy
-    catalog : common_ids.CatalogCategory 
+    catalog: common_ids.CatalogCategory
     dependsOn: set[DAppOrFamily] = set()
-    labels : dict[common_ids.Label,typing.Any] = {}
+    labels: dict[common_ids.Label, typing.Any] = {}
 
-    searchtext : str|None = None
+    searchtext: str | None = None
 
-    def __init__(self,*a,**kw):
+    def __init__(self, *a, **kw):
         """ Calculate labels; would like a computed and cached property,
             but Pydantic currently doesn't support that:
             https://github.com/samuelcolvin/pydantic/pull/2625
         """
-        super().__init__(*a,**kw)
+        super().__init__(*a, **kw)
         if not self.id:
-            self.id = typing.cast(principals.DAppId,self.__class__.__name__) 
+            self.id = typing.cast(principals.DAppId, self.__class__.__name__)
         if not self.description:
             self.description = str(self.id)
         if not self.searchtext:
@@ -40,36 +40,38 @@ class DAppOrFamily(NoCopyBaseModel):
             self.searchtext = self.searchtext.lower()
         self.labels = self.compute_labels()
 
-
     def __repr__(self):
         """ Normal display is excessively long """
         return f'{self.__class__.__name__}(id={self.id})'
 
-    def compute_labels(self) ->dict:
+    def compute_labels(self) -> dict:
         """ Compute and assign labels """
         # TODO: Compute labels from other attributes
-        return {common_ids.Label.id:self.id}
-    
+        return {common_ids.Label.id: self.id}
+
     def to_DAppOrFamily(self):
         """ convert DApp or DAppFamily to DAppOrFamily, which is the FastAPI ResponseModel  """
-        return DAppOrFamily(**self.dict()) # excess attributes are ignore
+        return DAppOrFamily(**self.dict())  # excess attributes are ignore
+
 
 DAppOrFamily.update_forward_refs()
 
+
 class DAppFamily(DAppOrFamily):
     """ A DAppFamily is a collection of DApps that can be subscribed together """
-    members : dict[principals.DAppId,DApp] = {}
+    members: dict[principals.DAppId, DApp] = {}
 
 
 @enum.unique
-class EstimatedCosts(str,enum.Enum):
+class EstimatedCosts(str, enum.Enum):
     """ Operations """
 
     free = 'free'
     low = 'low'
     medium = 'medium'
     high = 'high'
-    user = 'user dependent'    
+    user = 'user dependent'
+
 
 CostToWeight = {
     EstimatedCosts.free: 0.0,
@@ -79,41 +81,50 @@ CostToWeight = {
 }
 
 
+@enum.unique
+class Priviledges(str, enum.Enum):
+    """ Priviledges a DApp may enjoy """
+
+    system = 'system'  # System DApp, aka root
+    incoming_http = 'incoming_http'
+    outgoing_http = 'outgoing_http'
+    sensitive_data_read = 'sensitive_data_read'  # may read data that is designated sensitive in Schema
+
+
 class DApp(DAppOrFamily):
     class Config:
-        extra = 'allow' # DApps are free to use their own variables
-    
-    belongsTo: DAppFamily|None = None
-    references : list[relationships.Reference] = []
-    transforms_into : keys.DDHkey|None = None
-    estimatedCosts : EstimatedCosts = EstimatedCosts.free
+        extra = 'allow'  # DApps are free to use their own variables
 
+    belongsTo: DAppFamily | None = None
+    references: list[relationships.Reference] = []
+    transforms_into: keys.DDHkey | None = None
+    estimatedCosts: EstimatedCosts = EstimatedCosts.free
+    requested_priviledges: set[Priviledges] = set()
+    granted_priviledges: set[Priviledges] = pydantic.Field(
+        default=set(), const=True, description="priviledges actually granted, cannot be set")
 
-    def __init__(self,*a,**kw):
+    def __init__(self, *a, **kw):
         """ Add to family as member """
-        super().__init__(*a,**kw)
+        super().__init__(*a, **kw)
         if self.belongsTo:
             self.belongsTo.members[self.id] = self
-
+        self.granted_priviledges = self.requested_priviledges
 
     def __hash__(self):
         return hash(self.id)
 
-    def __eq__(self,other):
-        return (self.id == other.id) if isinstance(other,DApp) else False
-            
+    def __eq__(self, other):
+        return (self.id == other.id) if isinstance(other, DApp) else False
 
-    def get_schemas(self) -> dict[keys.DDHkey,schemas.AbstractSchema]:
+    def get_schemas(self) -> dict[keys.DDHkey, schemas.AbstractSchema]:
         """ Obtain initial schema for DApp """
         return {}
 
-
-   
     def get_references(self):
         """ return references; can be overwritten """
         return self.references
 
-    def add_reference(self,references: list[relationships.Reference]) -> DApp:
+    def add_reference(self, references: list[relationships.Reference]) -> DApp:
         """ add a reference and returns self for chaining """
         self.references.extend(references)
         return self
@@ -122,9 +133,9 @@ class DApp(DAppOrFamily):
         """ is the availability dependent on the user, e.g., for employee DApps.
             the concrete availability can be determined by .availability_for_user()
         """
-        return False 
+        return False
 
-    def availability_for_user(self,principal: principals.Principal) -> bool:
+    def availability_for_user(self, principal: principals.Principal) -> bool:
         """ Whether this DApp can be obtained by this user, for selection purposes only.
         """
         return True
@@ -137,7 +148,7 @@ class DApp(DAppOrFamily):
             cost = CostToWeight[self.estimatedCosts]
         return cost
 
-    def cost_for_user(self,principal: principals.Principal) -> float:
+    def cost_for_user(self, principal: principals.Principal) -> float:
         """ return cost of this DApp for a user, for selection purposes only.
         """
         return 0.0
@@ -146,23 +157,22 @@ class DApp(DAppOrFamily):
         """ get weight based on costs """
         return 1.0 + self.estimated_cost()
 
+
 DApp.update_forward_refs()
 
+
 class RunningDApp(NoCopyBaseModel):
-    id : str|None  = None # principals.DAppId causes Pydantic errors - I don't know why
-    dapp_version : versions.Version
-    schema_version : versions.Version
-    location : pydantic.AnyHttpUrl
+    id: str | None = None  # principals.DAppId causes Pydantic errors - I don't know why
+    dapp_version: versions.Version
+    schema_version: versions.Version
+    location: pydantic.AnyHttpUrl
+
 
 class ExecuteRequest(NoCopyBaseModel):
     """ This is the execution request passed between micro services """
-    op : typing.Any # nodes.Ops
-    access : permissions.Access
+    op: typing.Any  # nodes.Ops
+    access: permissions.Access
     transaction: transactions.Transaction
-    key_split : int|None = None
-    data : dict|None = None
-    q : str|None = None
-
-
-
- 
+    key_split: int | None = None
+    data: dict | None = None
+    q: str | None = None
