c4daa5ecd022852446b81a7cb23064fba4e27a3f
Gfeller Martin <mgf@acm.org>
2022-12-28 10:46:19 +0100

    Pydantic change https://github.com/pydantic/pydantic/pull/2193

--- utils/pydantic_utils.py
+++ utils/pydantic_utils.py
@@ -1,13 +1,12 @@
 """ Pydantic utilities and superclasses """
 
 from __future__ import annotations
-import pydantic 
+import pydantic
 import typing
 
 
 class NoCopyBaseModel(pydantic.BaseModel):
-    """ https://github.com/samuelcolvin/pydantic/issues/1246
-        https://github.com/samuelcolvin/pydantic/blob/52af9162068a06eed5b84176e987a534f6d9126a/pydantic/main.py#L574-L575
+    """ Default Model behavior
     """
     class Config:
         """ This forbids wrong keywords, preventing silly mistakes when defaulted
@@ -15,19 +14,20 @@ class NoCopyBaseModel(pydantic.BaseModel):
         """
         extra = 'forbid'
         underscore_attrs_are_private = True
+        copy_on_model_validation = 'none'  # see https://github.com/pydantic/pydantic/pull/2193
 
-    @classmethod
-    def validate(cls: typing.Type[pydantic.BaseModel], value: typing.Any) -> pydantic.BaseModel:
-        if isinstance(value, cls):
-            return value # don't copy!
-        else:
-            return super().validate(value) 
+    # @classmethod
+    # def validate(cls: typing.Type[pydantic.BaseModel], value: typing.Any) -> pydantic.BaseModel:
+    #     if isinstance(value, cls):
+    #         return value # don't copy!
+    #     else:
+    #         return super().validate(value)
 
     @classmethod
     def add_fields(cls, **field_definitions: typing.Any):
         """ Add fields in-place https://github.com/samuelcolvin/pydantic/issues/1937 """
         new_fields: dict[str, pydantic.fields.ModelField] = {}
-        new_annotations: dict[str, type|None] = {}
+        new_annotations: dict[str, type | None] = {}
 
         for f_name, f_def in field_definitions.items():
             if isinstance(f_def, tuple):
@@ -45,7 +45,8 @@ class NoCopyBaseModel(pydantic.BaseModel):
             if f_annotation:
                 new_annotations[f_name] = f_annotation
 
-            new_fields[f_name] = pydantic.fields.ModelField.infer(name=f_name, value=f_value, annotation=f_annotation, class_validators=None, config=cls.__config__)
+            new_fields[f_name] = pydantic.fields.ModelField.infer(
+                name=f_name, value=f_value, annotation=f_annotation, class_validators=None, config=cls.__config__)
 
         cls.__fields__.update(new_fields)
         cls.__annotations__.update(new_annotations)
