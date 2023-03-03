e544d884861d2e3c48274a45884a195919512104
Gfeller Martin <mgf@acm.org>
2023-03-03 14:54:14 +0100

    #17 TEST only

---
+++ test17.py
@@ -0,0 +1,26 @@
+from __future__ import annotations
+
+import typing
+import pydantic
+from fastapi.encoders import jsonable_encoder
+from core import keys
+from backend import persistable
+
+K = keys.DDHkey
+
+
+class Kdict(persistable.Persistable):
+
+    kd: dict[K, K] = {}
+
+
+kd = Kdict(kd={K('k1'): K('v1')})
+
+
+def test(kd):
+    j = jsonable_encoder(kd)
+    print(j)
+
+    j = kd.to_json()
+
+    kd2 = Kdict.from_json(j)
