1377a61d4541689db832f745357682d34b7ecc0f
Gfeller Martin <mgf@acm.org>
2023-05-20 15:57:34 +0200

    #23 WIP Schema Restrictions

--- core/restrictions.py
+++ core/restrictions.py
@@ -14,7 +14,11 @@ from . import (errors, versions, permissions, schemas, transactions)
 
 
 class Restriction(DDHbaseModel):
-    ...
+    def merge(self, other: Restriction) -> typing.Self:
+        """ return the stronger of self and other restrictions, creating a new combined 
+            restrictions.
+        """
+        return self
 
 
 SchemaRestriction = typing.ForwardRef('SchemaRestriction')
@@ -24,12 +28,25 @@ Restrictions = typing.ForwardRef('Restrictions')
 class Restrictions(DDHbaseModel):
     """ A collection of Restriction """
     restrictions: list[Restriction] = []
+    _by_name: dict[str, Restriction] = {}
+
+    def __init__(self, *a, **kw):
+        super().__init__(*a, **kw)
+        self._by_name = {r.__class__.__name__: r for r in self.restrictions}
 
     def merge(self, other: Restrictions) -> typing.Self:
         """ return the stronger of self and other restrictions, creating a new combined 
             restrictions.
         """
-        return self
+        if self._by_name == other._by_name:
+            return self
+        else:
+            s = set(self._by_name)
+            common = [self._by_name[common].merge(other._by_name[common]) for common in s.intersection(other._by_name)]
+            s1 = [self._by_name[n] for n in s.difference(other._by_name)]
+            s2 = [other._by_name[n] for n in set(other._by_name).difference(s)]
+            r = self.__class__(restrictions=common+s1+s2)
+            return r
 
 
 class SchemaRestriction(Restriction):
@@ -38,12 +55,35 @@ class SchemaRestriction(Restriction):
     may_overwrite: bool = pydantic.Field(
         default=False, description="restriction may be overwritten explicitly in lower schema")
 
+    def merge(self, other: SchemaRestriction) -> typing.Self:
+        """ return the stronger of self and other restrictions, creating a new combined 
+            restrictions.
+        """
+        if not other.may_overwrite:
+            return other
+        else:  # all other case are equal
+            return self
+
     def apply(self, schema: schemas.AbstractSchema, access, transaction, data):
         return data
 
 
 class MustReview(SchemaRestriction):
     by_roles: set[str] = set()
+
+    def merge(self, other: MustReview) -> typing.Self:
+        """ return the stronger of self and other restrictions, creating a new combined 
+            restrictions.
+        """
+        may_overwrite = self.may_overwrite and other.may_overwrite  # stronger
+        if self.by_roles == other.by_roles and self.may_overwrite == may_overwrite:
+            return self
+        elif not self.by_roles and other.may_overwrite == may_overwrite:
+            return other
+        elif not other.by_roles and self.may_overwrite == may_overwrite:
+            return self
+        else:  # combine the roles
+            return self.__class__(may_overwrite=may_overwrite, by_roles=self.by_roles | other.by_roles)
 
 
 class MustHaveSensitivites(SchemaRestriction):
---
+++ tests/test_restrictions.py
@@ -0,0 +1,42 @@
+import functools
+from core import restrictions
+import pytest
+
+
+def test_simple_restriction():
+    """ test that restriction merge properly"""
+    r1 = restrictions.MustHaveSensitivites()
+    r2 = restrictions.MustHaveSensitivites()
+    assert r1 == r2
+    assert r1.merge(r2) is r2
+    r3 = restrictions.MustHaveSensitivites(may_overwrite=True)
+    assert r1.merge(r3) is r1  # the non-overwriting one
+
+
+def test_attributed_restriction():
+    """ test that restriction merge properly"""
+    r1 = restrictions.MustReview()
+    r2 = restrictions.MustReview(by_roles={'boss'})
+    assert r1.merge(r2) is r2
+    r2a = restrictions.MustReview(may_overwrite=True, by_roles={'boss'})
+    assert r1.merge(r2a) is not r2a
+    assert r1.merge(r2a) == restrictions.MustReview(may_overwrite=False, by_roles={'boss'})
+    r3 = restrictions.MustReview(by_roles={'bigboss'})
+    assert r2.merge(r3) == restrictions.MustReview(by_roles={'boss', 'bigboss'})
+
+
+def test_restrictions():
+    r1 = restrictions.MustReview()
+    r2 = restrictions.MustReview(by_roles={'boss'})
+    r3 = restrictions.MustHaveSensitivites()
+    R1 = restrictions.Restrictions(restrictions=[r1])
+    R1a = restrictions.Restrictions(restrictions=[r1])
+    assert R1 == R1a
+    assert R1.merge(R1) is R1
+    assert R1.merge(R1a) is R1
+
+    R2 = restrictions.Restrictions(restrictions=[r2])
+    assert R1.merge(R2) == restrictions.Restrictions(restrictions=[r2])  # r1 is weaker than r2
+    R13 = restrictions.Restrictions(restrictions=[r1, r3])
+    assert R1.merge(R13) == restrictions.Restrictions(restrictions=[r1, r3])
+    assert R2.merge(R13) == restrictions.Restrictions(restrictions=[r2, r3])
