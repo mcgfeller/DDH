5c475809415ac95d0995e88b25e9050f03d03d6c
Gfeller Martin <mgf@acm.org>
2023-05-29 10:47:33 +0200

    #25 WIP Restrictions become Assignable

--- core/assignable.py
+++ core/assignable.py
@@ -4,9 +4,11 @@ from __future__ import annotations
 import enum
 import typing
 import abc
+import copy
 
 import pydantic
 from utils.pydantic_utils import DDHbaseModel
+from utils import utils
 
 from . import errors
 
@@ -14,3 +16,71 @@ from . import errors
 class Assignable(DDHbaseModel, typing.Hashable):
     class Config:
         frozen = True  # Assignables are not mutable, and we need a hash function to build  a set
+
+    may_overwrite: bool = pydantic.Field(
+        default=False, description="assignable may be overwritten explicitly in lower schema")
+    cancel: bool = pydantic.Field(
+        default=False, description="cancels this assignable in merge; set using ~assignable")
+
+    def __invert__(self) -> typing.Self:
+        """ invert the cancel flag """
+        d = self.dict()
+        d['cancel'] = True
+        return self.__class__(**d)
+
+    def merge(self, other: Assignable) -> typing.Self | None:
+        """ return the stronger of self and other assignables if self.may_overwrite,
+            or None if self.may_overwrite and cancels. 
+        """
+        if self.may_overwrite:
+            if other.cancel:
+                return None
+            else:
+                return other
+        else:  # all other case are equal
+            return self
+
+
+class Assignables(DDHbaseModel):
+    """ A collection of Assignable.
+        Assignable is not hashable, so we keep a list and build a dict with the class names. 
+    """
+    assignables: list[Assignable] = []
+    _by_name: dict[str, Assignable] = {}
+
+    def __init__(self, *a, **kw):
+        if a:  # shortcut to allow Assignable as args
+            kw['assignables'] = list(a)+kw.get('assignables', [])
+        super().__init__(**kw)
+        self._by_name = {r.__class__.__name__: r for r in self.assignables}
+
+    def __contains__(self, assignable: type[Assignable]) -> bool:
+        """ returns whether assignable class is in self """
+        return assignable.__name__ in self._by_name
+
+    def __eq__(self, other) -> bool:
+        """ must compare ._by_name as list order doesn't matter """
+        if isinstance(other, Assignables):
+            return self._by_name == other._by_name
+        else:
+            return False
+
+    def merge(self, other: Assignables) -> typing.Self:
+        """ return the stronger of self and other assignables, creating a new combined 
+            Assignables.
+        """
+        if self._by_name == other._by_name:
+            return self
+        else:  # merge those in common, then add those only in each set:
+            s1 = set(self._by_name)
+            s2 = set(other._by_name)
+            # merge, or cancel:
+            common = [r for common in s1 & s2 if (r := self._by_name[common].merge(other._by_name[common])) is not None]
+            r1 = [self._by_name[n] for n in s1 - s2]  # only in self
+            r2 = [other._by_name[n] for n in s2 - s1]  # only in other
+            r = self.__class__(assignables=common+r1+r2)
+            return r
+
+    def __add__(self, assignable: Assignable | list[Assignable]) -> typing.Self:
+        """ add assignable by merging """
+        return self.merge(self.__class__(assignables=utils.ensure_tuple(assignable)))
--- core/restrictions.py
+++ core/restrictions.py
@@ -12,35 +12,12 @@ import pydantic
 from utils.pydantic_utils import DDHbaseModel, tuple_key_to_str, str_to_tuple_key
 from utils import utils
 
-from . import (errors, versions, permissions, schemas, transactions)
+from . import (errors, versions, permissions, schemas, transactions, assignable)
 
 Tsubject = typing.TypeVar('Tsubject')  # subject of apply
 
 
-class Restriction(DDHbaseModel):
-
-    may_overwrite: bool = pydantic.Field(
-        default=False, description="restriction may be overwritten explicitly in lower schema")
-    cancel: bool = pydantic.Field(
-        default=False, description="cancels this restriction in merge; set using ~restriction")
-
-    def __invert__(self) -> typing.Self:
-        """ invert the cancel flag """
-        r = copy.copy(self)
-        r.cancel = not self.cancel
-        return r
-
-    def merge(self, other: Restriction) -> typing.Self | None:
-        """ return the stronger of self and other restrictions if self.may_overwrite,
-            or None if self.may_overwrite and cancels. 
-        """
-        if self.may_overwrite:
-            if other.cancel:
-                return None
-            else:
-                return other
-        else:  # all other case are equal
-            return self
+class Restriction(assignable.Assignable):
 
     def apply(self, subject: Tsubject, restrictions: Restrictions, *a, **kw) -> Tsubject:
         return subject
@@ -50,53 +27,14 @@ SchemaRestriction = typing.ForwardRef('SchemaRestriction')
 Restrictions = typing.ForwardRef('Restrictions')
 
 
-class Restrictions(DDHbaseModel):
+class Restrictions(assignable.Assignables):
     """ A collection of Restriction.
         Restriction is not hashable, so we keep a list and build a dict with the class names. 
     """
-    restrictions: list[Restriction] = []
-    _by_name: dict[str, Restriction] = {}
-
-    def __init__(self, *a, **kw):
-        if a:  # shortcut to allow Restriction as args
-            kw['restrictions'] = list(a)+kw.get('restrictions', [])
-        super().__init__(**kw)
-        self._by_name = {r.__class__.__name__: r for r in self.restrictions}
-
-    def __contains__(self, restriction: type[Restriction]) -> bool:
-        """ returns whether restriction class is in self """
-        return restriction.__name__ in self._by_name
-
-    def __eq__(self, other) -> bool:
-        """ must compare ._by_name as list order doesn't matter """
-        if isinstance(other, Restrictions):
-            return self._by_name == other._by_name
-        else:
-            return False
-
-    def merge(self, other: Restrictions) -> typing.Self:
-        """ return the stronger of self and other restrictions, creating a new combined 
-            Restrictions.
-        """
-        if self._by_name == other._by_name:
-            return self
-        else:  # merge those in common, then add those only in each set:
-            s1 = set(self._by_name)
-            s2 = set(other._by_name)
-            # merge, or cancel:
-            common = [r for common in s1 & s2 if (r := self._by_name[common].merge(other._by_name[common])) is not None]
-            r1 = [self._by_name[n] for n in s1 - s2]  # only in self
-            r2 = [other._by_name[n] for n in s2 - s1]  # only in other
-            r = self.__class__(restrictions=common+r1+r2)
-            return r
-
-    def __add__(self, restriction: Restriction | list[Restriction]) -> typing.Self:
-        """ add restriction by merging """
-        return self.merge(self.__class__(restrictions=utils.ensure_tuple(restriction)))
 
     def apply(self, restriction_class: type[Restriction], subject: Tsubject, *a, **kw) -> Tsubject:
         """ apply restrictions in turn """
-        for restriction in self.restrictions:
+        for restriction in self.assignables:
             if isinstance(restriction, restriction_class):
                 subject = restriction.apply(subject, self, *a, **kw)
         return subject
