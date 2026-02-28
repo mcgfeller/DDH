332bfaf45f3ceac4c178651bfbf477fdb90f038b
Gfeller Martin <mgf@acm.org>
2026-02-28 11:25:35 +0100

    #56 FIX Update to FastAPI 0.134.0 causes errors, need to declare Content-Type when using data = .model_dump_json()

--- core/dapp_proxy.py
+++ core/dapp_proxy.py
@@ -68,7 +68,7 @@ class DAppProxy(DDHbaseModel):
 
     async def execute(self, req: dapp_attrs.ExecuteRequest):
         """ forward execution request to DApp microservice """
-        resp = await self.running.client.post('execute', data=req.model_dump_json())
+        resp = await self.running.client.post('execute', data=req.model_dump_json(), headers={'Content-Type': 'application/json'})
         errors.DAppError.raise_from_response(resp)  # Pass error response to caller
         return resp.json()
 
